163a53422c2e31410ab1ccd8635dd27326a74032
minh0518 <rubics@naver.com>
2024-01-23 12:05:05 +0900

    refactor: 공통 컴포넌트 분리

--- src/app/(afterlogin)/presentation/upload/[id]/_component/CreatePresentation.tsx
+++ src/app/(afterlogin)/presentation/upload/[id]/_component/CreatePresentation.tsx
@@ -1,16 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import UploadPpt from './UploadPpt';
-import UploadTitle from './UploadTitle';
-import UploadScript from './UploadScript';
-import UploadTimer from './UploadTimer';
-import ControlButtons from './ControlButtons';
+
 import { PagesDataType } from '@/types/service';
-import styles from './CreatePresentation.module.scss';
-import UploadDday from './UploadDday';
-import Button from '@/app/_components/_elements/Button';
-import UploadMemo from './UploadMemo';
+
+import InputSection from './InputSection';
 
 const CreatePresentation = () => {
   const initialState: PagesDataType = {
@@ -24,52 +18,14 @@ const CreatePresentation = () => {
   const [currentPageIndex, setCurrpentPageIndex] = useState(0);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <UploadPpt
-          pptInfo={presentationData.scripts[currentPageIndex].ppt}
-          setPresentationData={setPresentationData}
-          currentPageIndex={currentPageIndex}
-        />
-        <ControlButtons
-          presentationData={presentationData}
-          setPresentationData={setPresentationData}
-          currentPageIndex={currentPageIndex}
-          setCurrpentPageIndex={setCurrpentPageIndex}
-          initialState={initialState}
-        />
-      </div>
-      <div className={styles.right}>
-        <div className={styles.inputSection}>
-          <UploadTitle
-            title={presentationData.title || ''}
-            setPresentationData={setPresentationData}
-          />
-          <UploadScript
-            script={presentationData.scripts[currentPageIndex].script || ''}
-            setPresentationData={setPresentationData}
-            currentPageIndex={currentPageIndex}
-          />
-          <UploadMemo
-            memo={presentationData.scripts[currentPageIndex].memo || ''}
-            setPresentationData={setPresentationData}
-            currentPageIndex={currentPageIndex}
-          />
-          <UploadDday
-            dDay={presentationData.dDay || ''}
-            setPresentationData={setPresentationData}
-          />
-          <UploadTimer
-            timer={presentationData.timer || ''}
-            setPresentationData={setPresentationData}
-          />
-        </div>
-        <div className={styles.saveButtons}>
-          <Button _content={'저장'} onClick={() => {}} />
-          <Button _content={'발표 연습 시작하기'} onClick={() => {}} />
-        </div>
-      </div>
-    </div>
+    <InputSection
+      presentationData={presentationData}
+      setPresentationData={setPresentationData}
+      currentPageIndex={currentPageIndex}
+      setCurrpentPageIndex={setCurrpentPageIndex}
+      initialState={initialState}
+      slug="new"
+    />
   );
 };
 
--- src/app/(afterlogin)/presentation/upload/[id]/_component/EditPresentation.tsx
+++ src/app/(afterlogin)/presentation/upload/[id]/_component/EditPresentation.tsx
@@ -1,18 +1,12 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import UploadPpt from './UploadPpt';
-import UploadTitle from './UploadTitle';
-import UploadScript from './UploadScript';
-import UploadTimer from './UploadTimer';
-import ControlButtons from './ControlButtons';
 
 import { PagesDataType } from '@/types/service';
-import styles from './CreatePresentation.module.scss';
-import UploadDday from './UploadDday';
+
 import { useGetPresentationData } from '../_hooks/presentation';
-import Button from '@/app/_components/_elements/Button';
-import UploadMemo from './UploadMemo';
+
+import InputSection from './InputSection';
 
 interface EditPresentationProps {
   slug: string;
@@ -46,53 +40,14 @@ const EditPresentation = ({ slug }: EditPresentationProps) => {
   }, [value]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <UploadPpt
-          pptInfo={presentationData.scripts[currentPageIndex].ppt}
-          setPresentationData={setPresentationData}
-          currentPageIndex={currentPageIndex}
-        />
-        <ControlButtons
-          presentationData={presentationData}
-          setPresentationData={setPresentationData}
-          currentPageIndex={currentPageIndex}
-          setCurrpentPageIndex={setCurrpentPageIndex}
-          initialState={initialState}
-          slug={slug}
-        />
-      </div>
-      <div className={styles.right}>
-        <div className={styles.inputSection}>
-          <UploadTitle
-            title={presentationData.title || ''}
-            setPresentationData={setPresentationData}
-          />
-          <UploadScript
-            script={presentationData.scripts[currentPageIndex].script || ''}
-            setPresentationData={setPresentationData}
-            currentPageIndex={currentPageIndex}
-          />
-          <UploadMemo
-            memo={presentationData.scripts[currentPageIndex].memo || ''}
-            setPresentationData={setPresentationData}
-            currentPageIndex={currentPageIndex}
-          />
-          <UploadDday
-            dDay={presentationData.dDay || ''}
-            setPresentationData={setPresentationData}
-          />
-          <UploadTimer
-            timer={presentationData.timer || ''}
-            setPresentationData={setPresentationData}
-          />
-        </div>
-        <div className={styles.saveButtons}>
-          <Button _content={'저장'} onClick={() => {}} />
-          <Button _content={'발표 연습 시작하기'} onClick={() => {}} />
-        </div>
-      </div>
-    </div>
+    <InputSection
+      presentationData={presentationData}
+      setPresentationData={setPresentationData}
+      currentPageIndex={currentPageIndex}
+      setCurrpentPageIndex={setCurrpentPageIndex}
+      slug={slug}
+      initialState={initialState}
+    />
   );
 };
 
