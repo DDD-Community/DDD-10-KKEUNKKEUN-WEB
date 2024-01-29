79fa33e433ee9ed982c262dd4805e782bb237f47
minh0518 <rubics@naver.com>
2024-01-29 13:58:27 +0900

    feat: 재사용 토스트 컴포넌트 구현(알람, 저장)

---
+++ src/app/_components/_modules/_modal/AlarmToast.tsx
@@ -0,0 +1,12 @@
+'use client';
+import styles from './AlarmToast.module.scss';
+
+const AlarmToast = () => {
+  return (
+    <div className={styles.modalContent}>
+      <p>알람</p>
+    </div>
+  );
+};
+
+export default AlarmToast;
---
+++ src/app/_components/_modules/_modal/SaveToast.tsx
@@ -0,0 +1,12 @@
+'use client';
+import styles from './SaveToast.module.scss';
+
+const SaveToast = () => {
+  return (
+    <div className={styles.modalContent}>
+      <p>저장이 완료되었습니다</p>
+    </div>
+  );
+};
+
+export default SaveToast;
