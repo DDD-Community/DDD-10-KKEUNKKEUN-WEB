23ce147551eac833f89006e79eee16d99223fbcc
minh0518 <rubics@naver.com>
2024-02-02 00:04:46 +0900

    refactor: mixin을 활용한 리팩토링

--- src/app/_components/_elements/TextArea.tsx
+++ src/app/_components/_elements/TextArea.tsx
@@ -6,16 +6,14 @@ import classNames from 'classnames';
 const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
   ({ theme, value, ...rest }, ref) => {
     let themeClassName: string | undefined;
-
     if (theme === 'presentation_memo') {
-      themeClassName = `${classNames([styles.presentationTextArea, styles.scriptMemo])}`;
+      themeClassName = classNames(styles.scriptMemo);
     }
     if (theme === 'presentation_script') {
-      themeClassName = `${classNames([
-        styles.presentationTextArea,
-        styles.scriptTextarea,
-        (value as string[]).length > 5000 && styles.warning,
-      ])}`;
+      themeClassName = classNames({
+        [styles.scriptTextarea]: true,
+        [styles.warning]: (value as string[]).length > 5000,
+      });
     }
 
     return <textarea ref={ref} className={themeClassName} {...rest} />;
