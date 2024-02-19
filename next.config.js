a96aa15840fa42c25bce4fc5527cbbd4090c0959
minh0518 <rubics@naver.com>
2024-02-19 13:41:14 +0900

    feat: 프록시 URL 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = {
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL_DEV}/accounts/:path*`,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
