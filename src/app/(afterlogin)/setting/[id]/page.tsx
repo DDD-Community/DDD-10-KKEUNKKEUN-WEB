341562dbb9e770b6a327c6411f77e757dfcb9d18
minh0518 <rubics@naver.com>
2024-02-20 15:31:17 +0900

    refactor: seetting 페이지 폴더 수정

---
+++ src/app/(afterlogin)/setting/[id]/page.tsx
@@ -0,0 +1,8 @@
+import DragSection from './_components/DragSection';
+import styles from './page.module.scss';
+
+const page = () => {
+  return <div className={styles.container}>{/* <DragSection /> */}</div>;
+};
+
+export default page;
