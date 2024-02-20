1eabb5a0f4510b35bd3ba0e4f9dfa9317bf72aa4
Joie-Kim <joie.huiju@gmail.com>
2024-02-20 23:34:04 +0900

    feat: 발표 연습 페이지 UI 작업 중..

--- src/app/(afterlogin)/practice/[id]/page.tsx
+++ src/app/(afterlogin)/practice/[id]/page.tsx
@@ -1,29 +1,42 @@
 'use client';
 
-import QRCode from 'react-qr-code';
-import Recorder from '../_components/Recorder';
-import ImageUploader from '@/app/_components/_modules/ImageUploader';
-import ImagePreview from '@/app/_components/_modules/ImagePreview';
-import useImageUploader from '@/app/_hooks/useImageUploader';
+import styles from './page.module.scss';
+import classNames from 'classnames/bind';
 
 export default function Page({ params }: { params: { id: string } }) {
-  const { selectedFiles, base64Strings, handleFileChange } = useImageUploader();
+  const cx = classNames.bind(styles);
+
   return (
-    <>
-      <div>id: {params.id}</div>
-      <div style={{ width: '100px', height: '100px' }}>
-        <QRCode
-          style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
-          value={'www.naver.com'}
-        />
+    <div className={styles.container}>
+      <div className={styles.contents}>
+        <section className={styles.presentation__box}>
+          <article className={styles.presentation}>img...</article>
+          <section className={styles.helper__box}>
+            <article className={cx(['helper', 'helper--next'])}>
+              <h4 className={styles.helper__title}>다음 슬라이드</h4>
+              <span className={cx(['helper__subtitle', 'helper__subtitle--next'])}>2/15</span>
+              <div>img...</div>
+            </article>
+            <article className={cx(['helper', 'helper--memo'])}>
+              <h4 className={styles.helper__title}>메모하기</h4>
+              <span className={cx(['helper__subtitle', 'helper__subtitle--memo'])}>
+                발표 연습 중 메모를 입력하면 녹음이 일시정지돼요.
+              </span>
+              <textarea />
+            </article>
+          </section>
+        </section>
+        <article className={styles.script__box}>
+          <p className={styles.script}>
+            발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표
+            내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표
+            내용 발표 내용 ... 발표 내용 발표 내용 ...발표 내용 발표 내용 ... 발표 내용 발표 내용
+            ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용
+            발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ...
+            발표 내용 발표 내용 ... 발표 내용 발표 내용 ...
+          </p>
+        </article>
       </div>
-      <div>
-        <Recorder />
-      </div>
-      <div>
-        <ImageUploader _handleFileChange={handleFileChange} />
-        <ImagePreview selectedFiles={selectedFiles} base64Strings={base64Strings} />
-      </div>
-    </>
+    </div>
   );
 }
--- src/app/(afterlogin)/practice/_components/PracticeNav.tsx
+++ src/app/(afterlogin)/practice/_components/PracticeNav.tsx
@@ -1,4 +1,4 @@
-import { combineClassName } from '@/app/_utils/style';
+import classNames from 'classnames/bind';
 
 import LogoIcon from '@/app/_svgs/LogoIcon';
 import RecordOnIcon from '../_svgs/RecordOnIcon';
@@ -7,21 +7,23 @@ import CloseIcon from '../_svgs/CloseIcon';
 import styles from './PracticeNav.module.scss';
 
 const PracticeNav = () => {
+  const cx = classNames.bind(styles);
+
   return (
     <nav className={styles.container}>
       <div className={styles.contents__box}>
-        <div className={combineClassName([styles.contents, styles.contents__left])}>
+        <div className={cx(['contents', 'contents--left'])}>
           <LogoIcon />
           <h3 className={styles.title}>발표이름 발표이름 발표이름 발표이름 발표이름 발표이름</h3>
         </div>
-        <div className={combineClassName([styles.contents, styles.contents__center])}>
+        <div className={cx(['contents', 'contents--center'])}>
           <RecordOnIcon />
           <em className={styles.division}></em>
           <h2>15:00</h2>
         </div>
-        <div className={combineClassName([styles.contents, styles.contents__right])}>
-          <button className={styles.action__next}>다음 페이지</button>
-          <button className={styles.action__close}>
+        <div className={cx(['contents', 'contents--right'])}>
+          <button className={cx('action--next')}>다음 페이지</button>
+          <button className={cx('action--close')}>
             <CloseIcon />
           </button>
         </div>
