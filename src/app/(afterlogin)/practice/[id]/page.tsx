3621d653bab0911d97c2b1628bb128b3d9311029
Joie-Kim <joie.huiju@gmail.com>
2024-02-21 23:50:02 +0900

    feat: 발표 연습 페이지 UI

--- src/app/(afterlogin)/practice/[id]/page.tsx
+++ src/app/(afterlogin)/practice/[id]/page.tsx
@@ -12,17 +12,24 @@ export default function Page({ params }: { params: { id: string } }) {
         <section className={styles.presentation__box}>
           <article className={styles.presentation}>img...</article>
           <section className={styles.helper__box}>
-            <article className={cx(['helper', 'helper--next'])}>
-              <h4 className={styles.helper__title}>다음 슬라이드</h4>
-              <span className={cx(['helper__subtitle', 'helper__subtitle--next'])}>2/15</span>
-              <div>img...</div>
+            <article className={styles.helper}>
+              <h4 className={styles.helper__title}>
+                다음 슬라이드
+                <span className={cx(['helper__subtitle', 'helper__subtitle--next'])}>2/15</span>
+              </h4>
+              <div className={styles.helper__item}>img...</div>
             </article>
-            <article className={cx(['helper', 'helper--memo'])}>
-              <h4 className={styles.helper__title}>메모하기</h4>
-              <span className={cx(['helper__subtitle', 'helper__subtitle--memo'])}>
-                발표 연습 중 메모를 입력하면 녹음이 일시정지돼요.
-              </span>
-              <textarea />
+            <article className={styles.helper}>
+              <h4 className={styles.helper__title}>
+                메모하기
+                <span className={cx(['helper__subtitle', 'helper__subtitle--memo'])}>
+                  발표 연습 중 메모를 입력하면 녹음이 일시정지돼요.
+                </span>
+              </h4>
+              <textarea
+                className={styles.helper__item}
+                placeholder="ex. 발표문 수정 사항, 목소리 크기 등에 대한 메모를 작성해 주세요."
+              />
             </article>
           </section>
         </section>
