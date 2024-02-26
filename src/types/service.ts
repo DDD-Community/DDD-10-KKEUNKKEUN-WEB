3ffcac593401d287dbe56f59cd0caee55c11265e
minh0518 <rubics@naver.com>
2024-02-26 23:19:16 +0900

    refactor: 타입에 JSDoc 작성

--- src/types/service.ts
+++ src/types/service.ts
@@ -35,26 +35,41 @@ export interface MockUploadDataType {
 }
 
 // service
+/** 유저 정보를 나타내는 객체 */
 export interface UploadDataType {
+  /** 발표 ID - URL에 라우팅 되는 값 */
   id?: number;
+  /** 현재 날짜로부터 deadlineDate까지 남은 일수  */
   dday?: number;
+  /** 최초 생성 시간  */
   createdAt?: Date;
+  /** 마지막 수정 시간  */
   modifiedAt?: Date;
+  /** 발표 제목  */
   title: string | null;
+  /** 총 발표 시간  */
   timeLimit: {
     hours: number | null;
     minutes: number | null;
   };
+  /** 중간 알림  */
   alertTime: {
     hours: number | null;
     minutes: number | null;
   };
+  /** 발표 날짜  */
   deadlineDate: Value;
+  /** ppt 페이지별 정보  */
   slides: {
+    /** DB Identity Sequence. 백엔드 조회용 */
     id?: number;
+    /** 이미지 파일 ID */
     imageFileId: number | null;
+    /** 이미지 파일 URL */
     imageFilePath: string | null;
+    /** 대본 */
     script: string | null;
+    /** 메모 */
     memo: string | null;
   }[];
 }
