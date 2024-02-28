57aa2038f2a357d86f3e33cf43b248704935e5eb
minh0518 <rubics@naver.com>
2024-02-28 21:09:39 +0900

    fix: 서버사이드 에러처리를 위한 로직 변경

--- src/app/(afterlogin)/setting/[id]/page.tsx
+++ src/app/(afterlogin)/setting/[id]/page.tsx
@@ -12,7 +12,8 @@ const page = async ({ params }: PageProps) => {
   const slug = Number(params.id);
 
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
+  // await queryClient.prefetchQuery({
+  await queryClient.fetchQuery({
     queryKey: ['setting', slug],
     queryFn: async () => {
       const response = await serverSettingApi.getPresentationSettingData(slug);
