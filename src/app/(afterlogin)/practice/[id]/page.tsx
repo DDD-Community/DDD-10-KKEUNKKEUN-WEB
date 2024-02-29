f7aa446fcdccf902c46d6e6587f89a7c3cc75cb7
Joie-Kim <joie.huiju@gmail.com>
2024-02-29 23:21:50 +0900

    feat: 발표 연습 상세 조회 API 연동

--- src/app/(afterlogin)/practice/[id]/page.tsx
+++ src/app/(afterlogin)/practice/[id]/page.tsx
@@ -7,38 +7,89 @@ import { useEffect, useState } from 'react';
 import useRecorder from '../_hooks/useRecorder';
 import Alert from '@/app/_components/_modules/_modal/Alert';
 import SpeechBubble from '@/app/_components/_modules/SpeechBubble';
+import { useQuery } from '@tanstack/react-query';
+import { PracticeDetail } from '@/types/service';
+import { PracticeService } from '@/services/client/practice';
+import Image from 'next/image';
 
 export default function Page({ params }: { params: { id: string } }) {
+  const id = Number(params.id);
+
+  // #region state
+  const [slideIdx, setSlideIdx] = useState(0);
   const modal = useToggle();
   const bubble = useToggle();
   const recorder = useRecorder();
+  // #endregion
 
+  //
   const cx = classNames.bind(styles);
 
-  useEffect(() => {
-    modal.onOpen();
-    recorder.getMedia();
-    recorder.processPermission();
-  }, []);
+  // #region query
+  const { isLoading, data } = useQuery<PracticeDetail>({
+    queryKey: ['practice', id],
+    queryFn: () => PracticeService.getPracticeDetail(Number(id)),
+  });
 
+  /** 슬라이드 페이징 문자열 */
+  const slidePaging = `${slideIdx}/${data?.slides.length ?? 0}`;
+
+  /** 마지막 슬라이드 여부 */
+  const isLastSlide = data?.slides.length === (slideIdx > 0 ? slideIdx - 1 : 0);
+  // #endregion
+
+  // useEffect(() => {
+  //   modal.onOpen();
+  //   recorder.getMedia();
+  //   recorder.processPermission();
+  // }, []);
+
+  // #region function
+
+  // #endregion
+
+  // #region event-handler
   const handleModalAction = () => {
-    recorder.startRecording();
-    modal.onClose();
-    bubble.onOpen();
+    // recorder.startRecording();
+    // modal.onClose();
+    // bubble.onOpen();
   };
+  // #endregion
 
   return (
     <div className={styles.container}>
       <div className={styles.contents}>
         <section className={styles.presentation__box}>
-          <article className={styles.presentation}>img...</article>
+          <article className={styles.presentation}>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_BASE_URL_CDN}/${data?.slides[0].imageFilePath}`}
+              alt={`slide-${0}`}
+              width={900}
+              height={510}
+              style={{ objectFit: 'contain', borderRadius: '16px' }}
+            />
+          </article>
           <section className={styles.helper__box}>
             <article className={styles.helper}>
               <h4 className={styles.helper__title}>
                 다음 슬라이드
-                <span className={cx(['helper__subtitle', 'helper__subtitle--next'])}>2/15</span>
+                <span className={cx(['helper__subtitle', 'helper__subtitle--next'])}>
+                  {slidePaging}
+                </span>
               </h4>
-              <div className={styles.helper__item}>img...</div>
+              <div className={styles.helper__item}>
+                {isLastSlide ? (
+                  <div>last ... </div>
+                ) : (
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_BASE_URL_CDN}/${data?.slides[0].imageFilePath}`}
+                    alt={`slide-${0}`}
+                    width={370}
+                    height={200}
+                    style={{ objectFit: 'contain', borderRadius: '16px' }}
+                  />
+                )}
+              </div>
             </article>
             <article className={styles.helper}>
               <h4 className={styles.helper__title}>
@@ -50,19 +101,13 @@ export default function Page({ params }: { params: { id: string } }) {
               <textarea
                 className={styles.helper__item}
                 placeholder="ex. 발표문 수정 사항, 목소리 크기 등에 대한 메모를 작성해 주세요."
+                defaultValue={data?.slides[0].memo ?? ''}
               />
             </article>
           </section>
         </section>
         <article className={styles.script__box}>
-          <p className={styles.script}>
-            발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표
-            내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표
-            내용 발표 내용 ... 발표 내용 발표 내용 ...발표 내용 발표 내용 ... 발표 내용 발표 내용
-            ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용
-            발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ... 발표 내용 발표 내용 ...
-            발표 내용 발표 내용 ... 발표 내용 발표 내용 ...
-          </p>
+          <p className={styles.script}>{data?.slides[0].script ?? ''}</p>
         </article>
       </div>
       <Alert
